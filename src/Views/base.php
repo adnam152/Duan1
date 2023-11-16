3187a1328485f8403b143ce9fb2f209ee2079a7b
Tuấn Nam <teddy.nam152@gmail.com>
2023-11-16 22:08:58 +0700

    update4

--- src/Views/base.php
+++ src/Views/base.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!--  -->
     <link rel="icon" href="/DA1/assets/files/assets/images/favicon.ico" type="image/x-icon"> <!-- icon -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
